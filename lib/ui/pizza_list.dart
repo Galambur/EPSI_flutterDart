8e896d4f2d1487a860e702dc33e6675808aceeff
Galambur <gaelle.derambure@epsi.fr>
2021-10-29 17:30:41 +0200

    TD3 Pizza details raccordé au clic

--- lib/ui/pizza_list.dart
+++ lib/ui/pizza_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pizzeria/models/pizza.dart';
 import 'package:pizzeria/models/pizza_data.dart';
+import 'package:pizzeria/ui/pizza_details.dart';
 
 class PizzaList extends StatefulWidget {
   @override
@@ -12,73 +13,96 @@ class _PizzaListState extends State<PizzaList> {
   List<Pizza> _pizzas = [];
 
   @override
-  void initState(){
+  void initState() {
     _pizzas = PizzaData.buildList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Nos pizzas'),
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(8.0),
-        itemCount: _pizzas.length,
-        itemBuilder: (context, index) {
-          return _buildRow(_pizzas[index]);
-        },
-      )
+        appBar: AppBar(
+          title: Text('Nos pizzas'),
+        ),
+        body: ListView.builder(
+          padding: const EdgeInsets.all(8.0),
+          itemCount: _pizzas.length,
+          itemBuilder: (context, index) {
+            return _buildRow(_pizzas[index]);
+          },
+        )
     );
   }
 
   _buildRow(Pizza pizza) {
     return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          bottom:Radius.circular(10.0), top: Radius.circular(2.0)),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(10.0), top: Radius.circular(2.0)),
         ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ListTile(
-            title: Text(pizza.title),
-            subtitle: Container(
-              padding: const EdgeInsets.all(4.0),
-              child: Text(pizza.garniture),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => PizzaDetails(pizza),
+                  ),
+                );
+              },
+              child: _buildPizzaDetails(pizza),
             ),
-            leading: Icon(Icons.local_pizza),
+            _buildBuyButton()
+          ],
+        )
+    );
+  }
+
+  _buildPizzaDetails(Pizza pizza) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListTile(
+          title: Text(pizza.title),
+          subtitle: Text(pizza.garniture),
+          leading: Icon(Icons.local_pizza),
+        ),
+        Image.asset(
+          'assets/images/pizzas/${pizza.image}',
+          height: 120,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.fitWidth,
+        ),
+        Container(
+          padding: const EdgeInsets.all(4.0),
+          child: Text(pizza.garniture),
+        )
+      ],
+    );
+  }
+
+  _buildBuyButton() {
+    return Row (
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor:
+              MaterialStateProperty.all<Color>(Colors.red.shade800)
           ),
-          Image.asset(
-            'assets/images/pizzas/${pizza.image}',
-            height: 120,
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.fitWidth,
-          ),
-          Text(pizza.garniture),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
+          child: Row(
             children: [
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor:
-                    MaterialStateProperty.all<Color>(Colors.red.shade800),
-                ),
-                child: Row(
-                  children: [
-                    Icon(Icons.shopping_cart),
-                    SizedBox(width: 5),
-                    Text("Commander"),
-                  ],
-                ),
-                onPressed: () {
-                  print('Commander une pizza');
-                },
-              )
+              Icon(Icons.shopping_cart),
+              SizedBox(width: 5),
+              Text("Commander"),
             ],
-          )
-        ],
-      )
+          ),
+          onPressed: () {
+            print('Commander une pizza');
+          },
+        )
+      ],
     );
   }
 }
